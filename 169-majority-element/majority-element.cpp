626f9d261204d19fbaf21c126abd080c489e3d01
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-01-28 22:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.48%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,12 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
         int count = 0, candidate = 0;
         for (int num : nums) {
+            // initialize candidate
             if (count == 0) {
                 candidate = num;
             }
+            // if num is same -- increase count
             if (num == candidate) {
                 count++;
             } else {
+                // else decrement counter
                 count--;
             }
         }
