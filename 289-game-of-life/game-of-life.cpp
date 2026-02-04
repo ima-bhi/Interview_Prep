1344c1ab119b8e674a1e611f286b28e1e8e77ea7
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-02-04 23:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.69%) - LeetSync

---
+++ 289-game-of-life/game-of-life.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int getLiveNeighbours(vector<vector<int>>& curr, int i, int j, int m,
+                          int n) {
+        int liveNeighbours = 0;
+        if (i > 0) {
+            // check the upper neighbour
+            if (curr[i - 1][j] == 1)
+                liveNeighbours++;
+        }
+        if (i < m - 1) {
+            // check the lower neighbour
+            if (curr[i + 1][j] == 1)
+                liveNeighbours++;
+        }
+        if (j > 0) {
+            // check the left neighbour
+            if (curr[i][j - 1] == 1)
+                liveNeighbours++;
+        }
+        if (j < n - 1) {
+            // check the right neighbour
+            if (curr[i][j + 1] == 1)
+                liveNeighbours++;
+        }
+
+        if (i > 0 && j > 0) {
+            // left diagnonal
+            if (curr[i - 1][j - 1] == 1)
+                liveNeighbours++;
+        }
+        if (i > 0 && j < n - 1) {
+            // right diagonal
+            if (curr[i - 1][j + 1] == 1)
+                liveNeighbours++;
+        }
+        if (i < m - 1 && j > 0) {
+            // bottom left
+            if (curr[i + 1][j - 1] == 1)
+                liveNeighbours++;
+        }
+        if (i < m - 1 && j < n - 1) {
+            // bottom right
+            if (curr[i + 1][j + 1] == 1)
+                liveNeighbours++;
+        }
+        return liveNeighbours;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        vector<vector<int>> curr = board;
+        int m = board.size();
+        int n = board[0].size();
+        // iterate over each cell ---
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // calculate length
+                int ln = getLiveNeighbours(curr, i, j, m, n);
+                // Dead cell to active if exact 3 live cells
+                if (board[i][j] == 0) {
+                    if (ln == 3) {
+                        board[i][j] = 1;
+                    }
+                } else if (board[i][j] == 1) {
+                    //<2 -> die
+                    //==2 || ==3 live
+                    //>3 ->die
+                    if (ln < 2 || ln > 3) {
+                        board[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+};
