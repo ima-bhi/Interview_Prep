e9ce05bb0c93a0913c79650f7b7496bf5cc5e3c3
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-02-07 22:07:40 +0530

    Time: 4 ms (43.30%) | Memory: 54.8 MB (98.41%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        // clean the array 
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;
+            }
+        }
+
+        // mark the value presence
+        for (int i = 0; i < n; ++i) {
+            int num = abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -abs(nums[num - 1]);
+            }
+        }
+
+        //find first missing +ve number 
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        //default
+        return n + 1;        
+    }
+};
