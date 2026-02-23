e9539be62aa65e95d3f6e320025ee667df2cc3fe
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-02-23 22:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (59.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
+        //initialize pointer
         int minm=prices[0],maxProfit=0;
         for(int i=1;i<n;i++){
-            //step 1: find min
+            //step 1: find minm,then maxProfit 
             minm=min(prices[i],minm);
-            int profit= prices[i]-minm;
-            maxProfit=max(profit,maxProfit);
+            maxProfit=max(prices[i]-minm,maxProfit);
         }
         return maxProfit;
     }
