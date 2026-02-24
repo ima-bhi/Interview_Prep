1e7ce231c39d1e8a269c95e31b22e523b8927040
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-02-24 17:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (48.10%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         int k=0;
         for(int i=0;i<n;i++){
+            //validate condition and push the value 
             if(nums[i]!=val){
                 nums[k++]=nums[i];
             }
