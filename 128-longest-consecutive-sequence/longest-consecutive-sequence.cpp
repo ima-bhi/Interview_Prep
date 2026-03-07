965d79a23deb69f1a3e871938699b9db59810957
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-03-07 23:58:51 +0530

    Time: 175 ms (6.41%) | Memory: 91.3 MB (8.50%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,36 +3,20 @@ public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty())
             return 0;
-
-        // store unique el
         set<int> st(nums.begin(), nums.end());
 
-        int maxCount = 1, count = 1;
-        bool first = true;
-        int prev = 0;
-
-        // Step 2: iterate using for-each loop
-        for (int curr : st) {
-            //first el entry
-            if (first) {
-                prev = curr;
-                first = false;
-                continue;
-            }
-
-            if (curr - prev == 1) {
-                count++;
-            } else {
+        int maxCount = 1;
+        for (int num : st) {
+            if (!st.count(num - 1)) {
+                int curr = num;
+                int count = 1;
+                while (st.count(curr + 1)) {
+                    curr++;
+                    count++;
+                }
                 maxCount = max(maxCount, count);
-                count = 1;
             }
-
-            prev = curr;
         }
-
-        // final update
-        maxCount = max(maxCount, count);
-
         return maxCount;
     }
 };
