9f1f9b26b40877875e62bcd169f49172acfcc22d
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-03-07 20:11:08 +0530

    Time: 3 ms (79.38%) | Memory: 10.5 MB (95.52%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-   int lengthOfLongestSubstring(string s) {
-    if (s.empty()) return 0;
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+            return 0;
 
-    int n = s.length();
-    string uniq(1, s[0]);
-    string maxm(1, s[0]);
+        int n = s.length();
+        string uniq(1, s[0]);
+        string maxm(1, s[0]);
 
-    for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) {
 
-        // if current char already exists in uniq
-        if (uniq.find(s[i]) != string::npos) {
-
-            // update max
-            if (uniq.size() > maxm.size()) {
-                maxm = uniq;
-            }
-
-            // remove characters up to and including duplicate
             int pos = uniq.find(s[i]);
-            uniq = uniq.substr(pos + 1);
+
+            // duplicate found
+            if (pos != string::npos) {
+                if (uniq.size() > maxm.size()) {
+                    maxm = uniq;
+                }
+                // remove characters up to duplicate
+                uniq.erase(0, pos + 1);
+            }
+            // add current character
+            uniq.push_back(s[i]);
         }
 
-        // add current char
-        uniq.push_back(s[i]);
+        // final comparison (VERY IMPORTANT)
+        if (uniq.size() > maxm.size()) {
+            maxm = uniq;
+        }
+
+        return maxm.size();
     }
-
-    // final comparison (VERY IMPORTANT)
-    if (uniq.size() > maxm.size()) {
-        maxm = uniq;
-    }
-
-    return maxm.size();
-}
-
 };
