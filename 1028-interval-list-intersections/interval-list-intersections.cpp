5a7ea4d00ed2251060f172b1a3d1721eef48a4ec
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-03-10 11:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (14.82%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList){
-        int m = firstList.size();
-        int n = secondList.size();
+        int m = firstList.size(),n = secondList.size();
         vector<vector<int>> intersections;
         int i=0, j=0;
-
         while(i<m && j<n){
-            int s1 = firstList[i][0]; // start of ith interval in FIRST array 
-            int s2 = secondList[j][0]; // start of jth interval in SECOND array
-
-            int e1 = firstList[i][1]; // end of ith interval in FIRST array
-            int e2 = secondList[j][1]; // start of jth interval in SECOND array
+            int s1 = firstList[i][0],s2 = secondList[j][0]; // start and end of firstList []
+            int e1 = firstList[i][1],e2 = secondList[j][1]; // start and end of secondList []
             
-            int start = max(s1, s2);
-            int end = min(e1, e2);
-
-            
-            if(start<=end)// 1 : check if interval 
-            {
-                // 2 : find interval and push 
-                intersections.push_back({start,end});
-            }
+            int start = max(s1, s2),end = min(e1, e2);
+            if(start<=end) intersections.push_back({start,end}); // pushing intersection in []
             // 3 : choose which pointer to increment
             if(e1<e2){
                 i++;
