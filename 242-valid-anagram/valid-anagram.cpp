a2c30058d8e5f868442a1bfbd533b39c91dc33e3
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-04-04 21:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // check if size not equal return false
         if (s.size()!=t.size()) return false;
+        // map for stored frequency as per s and t--
         unordered_map <char, int> umap;
         for (int i=0; i<s.size(); i++) {
             umap[s[i]]++;
             umap[t[i]]--;
         }
+
+        //if any element - freq > 0 return false
         for (auto it: umap) {
             if (it.second) return false;
         }
+        // default true
         return true;
     }
 };
