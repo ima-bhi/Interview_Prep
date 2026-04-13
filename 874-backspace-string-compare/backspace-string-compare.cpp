371479a79baca26df0c31ff191e9b65f3b3b71b7
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-04-13 17:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.91%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
+    string process(string s) {
+        int i = 0;
+        while (i < s.length()) {
+            if (s[i] != '#') {
+                i++;
+            } else {
+                if (i == 0) {
+                    s.erase(0, 1);
+                } else {
+                    s.erase(i - 1, 2);
+                    i--;
+                }
+            }
+        }
+        return s;
+    }
     bool backspaceCompare(string s, string t) {
-        stack<char> st;
-        for (char c : s) {
-            if (c == '#') {
-                if (!st.empty())
-                    st.pop();
-            } else
-                st.push(c);
+        if (process(s) == process(t)) {
+            return true;
+        } else {
+            return false;
         }
-        stack<char> st2;
-        for (char c : t) {
-            if (c == '#') {
-                if (!st2.empty())
-                    st2.pop();
-            } else
-                st2.push(c);
-        }
-        return st == st2;
     }
 };
