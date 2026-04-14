d23b97ee24fcd2467fd6a51f0c7c723b08537241
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-04-14 11:04:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (89.12%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> shortestToChar(string s, char c) {
         vector<int> occur;
         vector<int> ans;
-
+        // store occurence of el
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == c)
                 occur.push_back(i);
         }
+        //iterate main array and store min dist of coccurence for specific idx
         for (int i = 0; i < s.size(); i++) {
             int dist = INT_MAX;
             for (int j = 0; j < occur.size(); j++) {
