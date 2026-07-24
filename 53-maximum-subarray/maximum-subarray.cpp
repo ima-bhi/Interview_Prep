d95937ec16e8d58f30e88eace23f16fd69abb68c
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-07-24 09:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (81.52%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,10 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int larg = nums[0], curr = nums[0];
         for (int i = 1; i < nums.size(); i++) {
+            //edge case
             if (curr < 0) {
                 curr = 0;
             }
             curr = curr + nums[i];
+            //find larg
             larg = max(larg, curr);
         }
         return larg;
