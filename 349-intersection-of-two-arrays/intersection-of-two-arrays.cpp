13889eb3c264e5aa140ae0675c2c07209051d4b6
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-07-26 22:18:16 +0530

    Time: 4 ms (21.87%) | Memory: 14.3 MB (47.80%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         //set for uniq num
         unordered_set<int> st(nums1.begin(), nums1.end());
-        vector<int> answer;
+        vector<int> common;
         //iteate over num2 and check if element present or not.
         for (auto it : nums2) {
             if (st.count(it)) {
-                answer.push_back(it);
+                common.push_back(it);
                 st.erase(it);
             }
         }
-        return answer;
+        return common;
     }
 };
