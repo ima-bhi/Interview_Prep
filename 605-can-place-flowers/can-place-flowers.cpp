f32028eeccb2f13e8dfb30dde5f2dc33f64bd8c2
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-07-27 12:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (98.69%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        //it* arr
         for (int i = 0; i < flowerbed.size(); i++) {
+            // check left and right boundary to plant tree
             bool left = i == 0 || flowerbed[i - 1] == 0;
             bool right = i == flowerbed.size() - 1 || flowerbed[i + 1] == 0;
 
