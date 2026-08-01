9b1ce50f83b3ec119acfdf5c7241595fa5357760
im_abhi <73182571+ima-bhi@users.noreply.github.com>
2026-08-01 23:08:48 +0530

    Time: 27 ms (99.14%) | Memory: 45.5 MB (56.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // initalize map to store el
         unordered_map<int, int> mp;
         int sum = 0, ans = 0;
-        mp[sum] = 1;
+        // set 0 idx to 1 b/c array is contiguous
+        mp[0] = 1;
         for (auto it : nums) {
             sum += it;
             int find = sum - k;
             if (mp.find(find) != mp.end()) {
+                // store freq. of occured el
                 ans += mp[find];
             }
             mp[sum]++;
